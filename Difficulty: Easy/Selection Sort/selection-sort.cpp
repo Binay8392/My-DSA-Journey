82aefdcbf187318671444f4382fa368ba9530f9a
Binay8392 <binayparamanik3@gmail.com>
2026-07-22 22:47:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Selection Sort/selection-sort.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    void selectionSort(vector<int> &arr) {
+        // code here
+        for(int i=0;i<arr.size();i++)
+        {
+            for(int j=i;j<arr.size();j++)
+            {
+                if(arr[i]>arr[j]) swap(arr[i],arr[j]);
+            }
+        }
+        return ;
+    }
+};
