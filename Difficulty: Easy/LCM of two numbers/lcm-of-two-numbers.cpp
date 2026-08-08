28e5b12df8232070126018ba43a8087d95843a50
Binay8392 <binayparamanik3@gmail.com>
2026-08-08 06:54:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/LCM of two numbers/lcm-of-two-numbers.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+  int gcd(int a, int b) {
+    return (b == 0) ? a : gcd(b, a % b);
+}
+    int lcm(int a, int b) {
+        // code here
+        return (a/gcd(a,b)) * b;
+    }
+};
