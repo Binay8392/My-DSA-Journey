c61173fbe7626219d88b565f166599342e641169
Binay8392 <binayparamanik3@gmail.com>
2026-08-08 07:56:09 +0530

    Time: 0 ms (100%), Space: 7.8 MB (92.94%) - LeetHub

---
+++ 1006-clumsy-factorial/1006-clumsy-factorial.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int clumsy(int n) {
+       if (n <= 2)
+            return n;
+
+        if (n == 3)
+            return 6;
+            if (n == 4)
+            return 7;
+         if (n % 4 == 0)
+            return n + 1;
+        else if (n % 4 == 1)
+            return n + 2;
+        else if (n % 4 == 2)
+            return n + 2;
+        else
+            return n - 1;
+    }
+};
