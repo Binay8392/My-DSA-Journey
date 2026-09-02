652faea9b22297fc87f5b47b0e385caa7f115faa
Binay8392 <binayparamanik3@gmail.com>
2026-09-03 03:25:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse a String/reverse-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    string reverseString(string& s) {
+        // code here
+        int i=0;
+        int j=s.size()-1;
+        while(i<=j)
+        {
+            swap(s[i++],s[j--]);
+            
+        }
+        return s;
+    }
+};
